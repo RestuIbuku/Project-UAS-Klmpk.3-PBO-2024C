cac92ce4fabf52847120c3884c4bec71f3d6aad2
ccandra <candracan404@gmail.com>
2025-05-15 18:28:23 +0700

    tambah home HomeController.java

--- src/main/java/com/example/ProjectUASKlmpk3PBO2024C/controller/HomeController.java
+++ src/main/java/com/example/ProjectUASKlmpk3PBO2024C/controller/HomeController.java
@@ -6,3 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.ProjectUASKlmpk3PBO2024C.service.ParkiranService;
 import com.example.ProjectUASKlmpk3PBO2024C.service.TransaksiService;
+
+@Controller
+public class HomeController {
+    
+    @Autowired
+    private ParkiranService parkiranService;
+    
+    @Autowired
+    private TransaksiService transaksiService;
+    
+    @GetMapping("/")
+    public String home(Model model) {
+        // Menambahkan data untuk dashboard dengan penanganan null
+        model.addAttribute("totalKendaraan", parkiranService.getJumlahKendaraan());
+        model.addAttribute("pendapatanHariIni", transaksiService.getPendapatanHariIni() != null ? 
+            String.format("%,.0f", transaksiService.getPendapatanHariIni()) : "0");
+        model.addAttribute("totalTransaksi", transaksiService.getTotalTransaksi());
+        
+        return "home";
+    }
+}
