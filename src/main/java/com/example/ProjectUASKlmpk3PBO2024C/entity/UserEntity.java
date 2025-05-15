8afc0d282978239506c220b2c7a657e087d5f8db
ccandra <candracan404@gmail.com>
2025-05-15 10:52:43 +0700

    Update UserEntity.java

--- src/main/java/com/example/ProjectUASKlmpk3PBO2024C/entity/UserEntity.java
+++ src/main/java/com/example/ProjectUASKlmpk3PBO2024C/entity/UserEntity.java
@@ -1,3 +1,48 @@
 package com.example.ProjectUASKlmpk3PBO2024C.entity;
 
 import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nama;
+    private String noTelp;
+    private String jenisPengguna;
+
+    // Getter dan Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getJenisPengguna() {
+        return jenisPengguna;
+    }
+
+    public void setJenisPengguna(String jenisPengguna) {
+        this.jenisPengguna = jenisPengguna;
+    }
+}
